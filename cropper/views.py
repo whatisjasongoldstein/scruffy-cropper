1979fe87af7e6f30ad0de3b1e626915ec2d1d3bc
Jason Goldstein <jason@betheshoe.com>
2012-12-01 18:52:39 -0600

    Widget, and smarter redirection

--- cropper/views.py
+++ cropper/views.py
@@ -8,7 +8,7 @@ from cropper.models import Crop
 from cropper.forms import CropImageForm
 
 
-def create_crop(request, app, model_name, object_id, field, template='cropper/crop.html', delete_redirect=None):
+def create_crop(request, app, model_name, object_id, field, template='cropper/crop.html', post_save_redirect=None):
     """ Create a crop, or edit an existing one. This is an example of a wrapping view.
 
     Notice there is no security around this view. I would not call it directly without a wrapper
@@ -24,7 +24,8 @@ def create_crop(request, app, model_name, object_id, field, template='cropper/cr
     if 'delete' in request.POST:
         crop = Crop.objects.filter(object_id=obj.id, content_type=ContentType.objects.get_for_model(obj), field=field)
         crop.delete()
-        return redirect(delete_redirect or '/')
+        messages.add_message(request, messages.SUCCESS, 'No crop? No problem..')
+        return redirect(post_save_redirect or '/')
 
     # Get any existing crop coordinates to pass along
     coordinates = Crop.objects.get_or_create(object_id = obj.id, 
@@ -39,8 +40,9 @@ def create_crop(request, app, model_name, object_id, field, template='cropper/cr
             crop = form.save()
             coordinates = crop.coordinates
             messages.add_message(request, messages.SUCCESS, 'Crop saved! That was easy.')
+            return redirect(post_save_redirect or '/')
         else:
-            messages.add_message(request, messages.ERROR, "WTF? Something is wrong.")
+            messages.add_message(request, messages.ERROR, "Not a valid crop.")
 
     return render(request, template, {
             'form': form,
---
+++ cropper/widgets.py
@@ -0,0 +1,75 @@
+from django.forms.widgets import ClearableFileInput, CheckboxInput
+from django.utils.safestring import mark_safe
+from django.template import Template, Context
+
+
+def _snippet(template="", context={}):
+    """ Simple template render """
+    t = Template(template)
+    c = Context(context)
+    html = t.render(c)
+    return html
+
+
+class CropImageWidget(ClearableFileInput):
+    """ ClearableFileInput with thumbnails and crop. """
+
+    def _crop_button(self, substitutions):
+        link = self.attrs.get('data-crop-url')
+        if not link:
+            return ""
+        return """<a class="button" href="%s">Crop</a>""" % link
+
+    def render(self, name, value, attrs=None):
+
+        substitutions = {
+            'initial_text': self.initial_text,
+            'input_text': self.input_text,
+            'clear_template': '',
+            'clear_checkbox_label': self.clear_checkbox_label,
+        }
+
+        substitutions['input'] = super(ClearableFileInput, self).render(name, value, attrs)
+        img = None
+        if value and hasattr(value, "url"):
+            substitutions['initial'] = '<a class="thumbTxt" href="%(link)s">%(txt)s</a>' % {
+                'link':value.url, 'txt':value.name[:20] }
+
+            img = _snippet(u'{% load thumbnail %}<img src="{% thumbnail photo "75x75" %}">', { 'photo': value })
+
+            if not self.is_required:
+                checkbox_name = self.clear_checkbox_name(name)
+                checkbox_id = self.clear_checkbox_id(checkbox_name)
+                substitutions['clear_checkbox_name'] = checkbox_name
+                substitutions['clear_checkbox_id'] = checkbox_id
+                substitutions['clear'] = CheckboxInput().render(checkbox_name, False, attrs={'id': checkbox_id})
+                substitutions['clear_template'] = self.template_with_clear % substitutions
+
+        styleable_input = '<b class="wrap_file_input">Select File %s</b><i class="current"></i>' % substitutions['input']
+        
+        clear_ui = "" # None by default
+        if not self.is_required and substitutions.get('clear'):
+            clear_ui = '<span>Clear %s</span>' % substitutions['clear']
+
+        output_list = ['<span class="imgWidget">',]
+        if substitutions.get('initial'):
+            output_list += [
+                '<b class="thumb">',
+                    img,
+                '</b>',
+                '<i class="context">',
+                        '<span>%s</span>' % substitutions['initial'],
+                        '<span>Change {1} {0}</span>'.format(styleable_input, self._crop_button(substitutions)),
+                        clear_ui,
+                '</i>',
+            ]
+        else:
+            output_list += [
+                styleable_input,
+            ]
+
+        output_list += ['</span>',] # close wrapper
+        output = u"".join(output_list)
+
+        return mark_safe(output)
+
