3aff9253426552135a83f49adf9a5e2e480b995b
Jason Goldstein <jason@betheshoe.com>
2013-01-13 18:20:18 -0600

    Fix setup again

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 
 setup(
     name='Scruffy Cropper',
-    version="0.2.1",
+    version="0.2.2",
     author='Jason Goldstein',
     author_email='jason@betheshoe.com',
     url='https://bitbucket.org/whatisjasongoldstein/django-cropper',
     packages=['cropper', 'cropper.migrations', ],
+    package_data={ 'cropper': ['templates/*',] },
     description='Generic cropping utility for Django, plays nicely with any imagefield.',
     long_description=open('README.markdown').read(),
 )
