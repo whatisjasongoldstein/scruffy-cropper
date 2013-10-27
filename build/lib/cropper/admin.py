41a0f1c55fd2c65010120de341fd29ebbad2a8e6
Jason Goldstein <jason@betheshoe.com>
2013-10-27 13:29:08 -0400

    Clear our setup problems.

--- build/lib/cropper/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from cropper.models import Crop
-
-class CropAdmin(admin.ModelAdmin):
-    list_display = ['content_object', 'content_type', 'object_id', 'field', 'image', 'coordinates',]
-    list_filter = ['content_type', 'field']
-
-admin.site.register(Crop, CropAdmin)
